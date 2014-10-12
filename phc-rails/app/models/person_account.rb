eb4f867a49aa171d89f9a6b2e0b400db0ae5ce26
Howard Chen <howardxchen@gmail.com>
2014-10-12 14:09:22 -0700

    Add annotation

--- phc-rails/app/models/person_account.rb
+++ phc-rails/app/models/person_account.rb
@@ -1,2 +1,14 @@
+# == Schema Information
+#
+# Table name: person_accounts
+#
+#  id         :integer          not null, primary key
+#  first_name :string(255)
+#  last_name  :string(255)
+#  birthday   :datetime
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class PersonAccount < ActiveRecord::Base
 end
