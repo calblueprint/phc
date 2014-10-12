7568352d043b3f9b7c4e377ba303c27f83e1d6b6
Howard Chen <howardxchen@gmail.com>
2014-10-12 14:09:03 -0700

    Create person's account

---
+++ phc-rails/app/models/person_account.rb
@@ -0,0 +1,2 @@
+class PersonAccount < ActiveRecord::Base
+end
--- phc-rails/db/schema.rb
+++ phc-rails/db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141010012107) do
+ActiveRecord::Schema.define(version: 20141012210700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "last_modifieds", force: true do |t|
     t.datetime "last_modified_datetime"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "person_accounts", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "birthday"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
