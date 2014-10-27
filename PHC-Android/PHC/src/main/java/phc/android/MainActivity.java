4e22d0edab89a2ad38a2cb32a29c8f03444dd796
Jason Won <jason.j.won@gmail.com>
2014-10-26 18:14:12 -0700

    Modified action bar, changed Fragments/Activities to the non-support library ones, added a fragment for the sidebar, started styling the sidebar.

--- PHC-Android/PHC/src/main/java/phc/android/MainActivity.java
+++ PHC-Android/PHC/src/main/java/phc/android/MainActivity.java
@@ -1,5 +1,6 @@
 package phc.android;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -8,7 +9,7 @@ import android.view.MenuItem;
 import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- PHC-Android/PHC/src/main/java/phc/android/SideBarFragment.java
+++ PHC-Android/PHC/src/main/java/phc/android/SideBarFragment.java
@@ -14,19 +14,19 @@ import android.widget.LinearLayout;
 import android.widget.ScrollView;
 import android.widget.Spinner;
 
-public class SelectServicesFragment extends Fragment {
+public class SideBarFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_select_services, container, false);
+        View view = inflater.inflate(R.layout.fragment_side_bar, container, false);
 
         // Grab list of all services offered for the current event from Salesforce DB
         Resources res = getResources();
-        String[] services = res.getStringArray(R.array.services_array);
+        String[] services = res.getStringArray(R.array.sidebar);
 
         // Dynamically populate linear layout with checkboxes for each service
-        LinearLayout ll = (LinearLayout) view.findViewById(R.id.services_list);
+        LinearLayout ll = (LinearLayout) view.findViewById(R.id.sidebar_list);
         for(int i = 0; i < services.length; i++){
             CheckBox cb = new CheckBox(getActivity());
             cb.setLayoutParams(new LinearLayout.LayoutParams(
