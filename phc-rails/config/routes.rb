86a5ef6bfa7e423bf40a77b37129503b2279e1cd
Howard Chen <howardxchen@gmail.com>
2014-11-02 04:04:23 -0800

    Create api routing constraints

--- phc-rails/config/routes.rb
+++ phc-rails/config/routes.rb
@@ -1,4 +1,13 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
   resources :accounts
   get 'pull', to: 'accounts#pull'
+
+  namespace :api, defaults: { format: :json },
+                  constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1,
+          constraints: ApiConstraints.new(version: 1, default: true) do
+    end
+  end
 end
