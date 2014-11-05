d98e8b3cf83fbc9a22db5bbeb374f5b1b5f3df3b
Byron Zhang <byron.yi.zhang@gmail.com>
2014-11-05 02:36:01 -0800

    optimize imports

--- PHC-Android/PHC/src/main/java/phc/android/SuccessFragment.java
+++ PHC-Android/PHC/src/main/java/phc/android/SuccessFragment.java
@@ -1,7 +1,6 @@
 package phc.android;
 
 import android.app.Fragment;
-import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
