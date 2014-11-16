d8c344cd5d27f92d9ed73683f12414692f1b40e0
Nishant Desai <nishantdesai@berkeley.edu>
2014-11-15 16:49:07 -0800

    Save search params to sharedprefs

--- PHC-Android/PHC/src/main/java/phc/android/SearchFragment.java
+++ PHC-Android/PHC/src/main/java/phc/android/SearchFragment.java
@@ -3,12 +3,14 @@ package phc.android;
 import android.app.Fragment;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -18,13 +20,24 @@ import android.widget.TextView;
  * and allows the user to go back to activity_register another client.
  */
 public class SearchFragment extends Fragment {
+    public static final String SEARCH_PARAMETERS = "SearchParametersFile";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
         Button submitSearchButton = (Button) view.findViewById(R.id.button_submit_search);
+        final EditText firstName = (EditText)view.findViewById(R.id.first_name);
+        final EditText lastName = (EditText) view.findViewById(R.id.last_name);
         submitSearchButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                //Save parameters to SharedPrefs to access from the next activity.
+                SharedPreferences searchPrameters = getActivity().getSharedPreferences(SEARCH_PARAMETERS, 0);
+                SharedPreferences.Editor searchParametersEditor = searchPrameters.edit();
+                searchParametersEditor.putString("firstName", firstName.getText().toString());
+                searchParametersEditor.putString("lastName", lastName.getText().toString());
+                searchParametersEditor.commit();
+
                 SearchResultsFragment newFragment = new SearchResultsFragment();
                 FragmentTransaction transaction = getFragmentManager().beginTransaction();
                 transaction.replace(R.id.registration_fragment_container, newFragment, getResources().getString(R.string.sidebar_search));
