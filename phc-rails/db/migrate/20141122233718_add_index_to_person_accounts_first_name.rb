672c61455fdbcd83537fdd9b871a635f8bc558cb
Howard Chen <howardxchen@gmail.com>
2014-11-22 15:46:50 -0800

    Fix indentation

--- phc-rails/db/migrate/20141122233718_add_index_to_person_accounts_first_name.rb
+++ phc-rails/db/migrate/20141122233718_add_index_to_person_accounts_first_name.rb
@@ -1,6 +1,6 @@
 class AddIndexToPersonAccountsFirstName < ActiveRecord::Migration
   def change
-      add_index :person_accounts, :first_name
-      add_index :person_accounts, :last_name
+    add_index :person_accounts, :first_name
+    add_index :person_accounts, :last_name
   end
 end
