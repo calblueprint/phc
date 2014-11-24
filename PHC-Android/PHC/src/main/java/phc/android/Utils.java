293c6145dc1f4b868a490d9dad3fa738769111bf
Byron Zhang <byron.yi.zhang@gmail.com>
2014-11-24 02:49:58 -0800

    add utils class for ID generator. move key conversion method to utils class as well.

---
+++ PHC-Android/PHC/src/main/java/phc/android/Utils.java
@@ -0,0 +1,38 @@
+package phc.android;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * Created by Byronium on 11/23/14.
+ */
+public class Utils {
+    private static final AtomicInteger sNextGeneratedId = new AtomicInteger(1);
+
+    /**
+     * Generate a unique ID.
+     * This value will not collide with ID values generated at build time by aapt for R.id.
+     * @return a generated ID value
+     */
+    public static int generateViewId() {
+        for (;;) {
+            final int result = sNextGeneratedId.get();
+            // aapt-generated IDs have the high byte nonzero; clamp to the range under that.
+            int newValue = result + 1;
+            if (newValue > 0x00FFFFFF) newValue = 1; // Roll over to 1, not 0.
+            if (sNextGeneratedId.compareAndSet(result, newValue)) {
+                return result;
+            }
+        }
+    }
+
+    /**
+     * Takes in a string ID and converts it to key format
+     * used in the SalesForce database via the following changes:
+     *   - add "__c" to the end of the field
+     *   - replace nonalphanumeric characters (e.g. "/" and "-") with "_".
+     */
+    public String keyToKeyConverter(String key){
+        key = key.replaceAll("[\\W\\s]","_") + "__c";
+        return key;
+    }
+}
