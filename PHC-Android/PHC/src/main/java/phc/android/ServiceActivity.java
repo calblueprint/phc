37a2cf053824df5f0d9c07ae8ef9304362fc212c
Deepak Warrier <dwarrier@gmail.com>
2014-12-09 09:17:38 -0800

    rename services layout xml

--- PHC-Android/PHC/src/main/java/phc/android/ServiceActivity.java
+++ PHC-Android/PHC/src/main/java/phc/android/ServiceActivity.java
@@ -65,7 +65,7 @@ public class ServiceActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.service);
+        setContentView(R.layout.activity_service);
         ActionBar actionbar = getActionBar();
         actionbar.setDisplayHomeAsUpEnabled(true);
 
