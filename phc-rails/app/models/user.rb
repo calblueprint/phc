e68c5649cfc40cadcd0d2a9c503528f9c8a1898b
Alton Zheng-Xie <altonzheng@berkeley.edu>
2015-02-05 18:48:18 -0800

    Migrations

---
+++ phc-rails/app/models/user.rb
@@ -0,0 +1,14 @@
+class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 225 },
+                                    format: { with: VALID_EMAIL_REGEX },
+                                    uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, length: { minimum: 6 }
+
+
+end
---
+++ phc-rails/db/migrate/20150206021804_add_index_to_user_email.rb
@@ -0,0 +1,5 @@
+class AddIndexToUserEmail < ActiveRecord::Migration
+  def change
+    add_index :users, :email, unique: true
+  end
+end
