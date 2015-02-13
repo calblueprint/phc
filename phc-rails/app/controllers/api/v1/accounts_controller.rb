a4277ccf07e78473763c7d7b4021fcd11b77bd84
Alton Zheng-Xie <altonzheng@berkeley.edu>
2015-02-12 21:05:12 -0800

    Auth tokens not being passed in headers but oh well

--- phc-rails/app/controllers/api/v1/accounts_controller.rb
+++ phc-rails/app/controllers/api/v1/accounts_controller.rb
@@ -2,7 +2,12 @@ class Api::V1::AccountsController < ApplicationController
   respond_to :json
 
   def search
-    if request.headers["AuthToken"].eql? ENV["AuthToken"]
+    if request.headers["auth_token"] && request.headers["user_id"]
+
+
+      ##### TODO: Check Authentication Tokens!!! ######
+
+
       first_name = request.headers["FirstName"]
       last_name = request.headers["LastName"]
       fhash = { first_name: first_name }
@@ -10,7 +15,7 @@ class Api::V1::AccountsController < ApplicationController
       res = PersonAccount.fuzzy_search(fhash, false).fuzzy_search(lhash, false)
       respond_with res
     else
-      respond_with []
+      render :json => request.headers["auth_token"]
     end
   end
 
