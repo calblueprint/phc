8ed06e57620c32593f6a1dd152627f98036b8409
Tony Wu <tony.wu@berkeley.edu>
2015-02-26 21:52:09 -0800

    Change version number to 1.0.2

---
+++ PHC-Android/PHC/src/main/java/phc/android/Networking/RequestManager.java
@@ -0,0 +1,16 @@
+package phc.android.Networking;
+
+/**
+ * Created by tonywu on 2/24/15.
+ */
+public class RequestManager {
+
+    //TODO: Change to heroku url when rails code pushed to heroku
+    private static final String BASE_URL = "localhost:3000/";
+
+    private static final String LOGIN_ENDPOINT = "login";
+
+    private static final String SEARCH_ENDPOINT = "api/v1/search";
+
+
+}
