4ee2c8b28ac03e13ccc4d2d7316e76a9be169a88
Daniel Li <danielmxli@gmail.com>
2015-02-26 22:51:44 -0800

    Refactoring CheckoutScanner

--- PHC-Android/PHC/src/main/java/phc/android/Checkout/CheckoutScannerFragment.java
+++ PHC-Android/PHC/src/main/java/phc/android/Checkout/CheckoutScannerFragment.java
@@ -4,14 +4,6 @@ package phc.android.Checkout;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.app.Fragment;
-import android.util.Log;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.RadioGroup;
-
 import phc.android.R;
 import phc.android.Services.ScannerFragment;
 
@@ -22,40 +14,6 @@ import phc.android.Services.ScannerFragment;
 public class CheckoutScannerFragment extends ScannerFragment {
     /* Name for logs and fragment transaction code */
     public final static String TAG = "CheckoutScannerFragment";
-
-
-    @Override
-
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState){
-        /* Inflate the layout for this fragment and set up view */
-        View view = setupView(inflater, container);
-        return view;
-    }
-
-    @Override
-    protected View setupView(LayoutInflater inflater, ViewGroup container){
-        View view = inflater.inflate(R.layout.fragment_checkout_scanner, container, false);
-
-        mScanButton = (Button) view.findViewById(R.id.checkout_start_scan);
-        mScanButton.setOnClickListener(new ScannerFragment.ScanListener());
-
-        setupButtons(view);
-
-        return view;
-    }
-
-    @Override
-    protected void setupButtons(View view){
-
-        mCodeInput = (EditText) view.findViewById(R.id.checkout_scanner_code_input);
-        mCodeInput.addTextChangedListener(new ScannerFragment.InputTextWatcher());
-
-        mCodeInputSubmitButton = (Button) view.findViewById(R.id.checkout_scanner_submit);
-        mCodeInputSubmitButton.setOnClickListener(new ScannerFragment.InputSubmitListener());
-        setInputSubmitButton();
-    }
-
 
     /**
      * Sets up the view for the user to confirm
