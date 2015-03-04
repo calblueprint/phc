5f0e7f360c066dddaad465f1aadfebfb6845aedc
Alton Zheng-Xie <altonzheng@berkeley.edu>
2015-03-03 21:09:08 -0800

    Fuzzy searching working

--- phc-rails/app/controllers/api/v1/accounts_controller.rb
+++ phc-rails/app/controllers/api/v1/accounts_controller.rb
@@ -3,13 +3,15 @@ class Api::V1::AccountsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def search
+    # ActiveRecord::Base.connection.execute("SELECT set_limit(0.001);")
     auth_token = request.headers["HTTP_AUTH_TOKEN"]
     user_id = request.headers["HTTP_USER_ID"]
     if auth_token && user_id
       if user_authenticated?(user_id, auth_token)
         first_name = request.headers["HTTP_FIRSTNAME"]
         last_name = request.headers["HTTP_LASTNAME"]
-        result = PersonAccount.fuzzy_search({first_name: first_name}, false).fuzzy_search({last_name: last_name}, false)
+        byebug
+        result = PersonAccount.fuzzy_search({first_name: first_name, last_name: last_name}, false)
         respond_with result
       end
     else
@@ -27,12 +29,12 @@ class Api::V1::AccountsController < ApplicationController
         person.last_name = params[:last_name]
         person.sf_id = request.headers[:salesforceid]
         person.save
-        respond_with "Successfully saved user!", status: 200, :location => root_url
+        respond_with "Successfully saved user!", status: 200, location: root_url
 
         ##### TODO: POST TO SALESFORCE #####
       end
     else
-      respond_with "Error: Please include authentication token.", status: 401, location => root_ur
+      respond_with "Error: Please include authentication token.", status: 401, location: root_url
     end
 
   end
