09d7475c9ce9aa8882d50df65e7f76798f372873
Byron Zhang <byron.yi.zhang@gmail.com>
2015-03-09 18:09:33 -0700

    tweak sidebar name and documentation

--- PHC-Android/PHC/src/main/java/phc/android/Sidebar/CheckinSideBarFragment.java
+++ PHC-Android/PHC/src/main/java/phc/android/Sidebar/CheckinSideBarFragment.java
@@ -23,10 +23,9 @@ public class CheckinSideBarFragment extends CheckinFragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_checkin_side_bar, container, false);
 
-        // Grab list of all services offered for the current event from Salesforce DB
         Resources res = getResources();
         final String[] elements = res.getStringArray(R.array.checkin_sidebar);
-        LinearLayout sidebarList = (LinearLayout) view.findViewById(R.id.checkin_sidebar);
+        LinearLayout sidebarList = (LinearLayout) view.findViewById(R.id.checkin_sidebar_list);
 
         // Dynamically add sidebar buttons to sidebar
         for (String element : elements) {
@@ -41,6 +40,7 @@ public class CheckinSideBarFragment extends CheckinFragment {
             button.setTextColor(getResources().getColor(R.color.black));
             button.setTextSize(getResources().getDimensionPixelSize(R.dimen.sidebar_button_size));
             button.setTag(tag);
+            // set onClickListener if fragment has been visited already (so can click to go back)
             button.setOnClickListener(new View.OnClickListener() {
 
                 @Override
