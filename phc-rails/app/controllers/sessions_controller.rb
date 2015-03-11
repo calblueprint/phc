e1e04ca35f4e08418e7a2e4da45e845fd7a5bf2e
Alton Zheng-Xie <altonzheng@berkeley.edu>
2015-03-10 20:50:56 -0700

    Make error messages more informative

--- phc-rails/app/controllers/sessions_controller.rb
+++ phc-rails/app/controllers/sessions_controller.rb
@@ -9,11 +9,11 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:email].downcase)
     if user && user.authenticate(params[:password])
       auth_token = user.remember
-      data = { user_id: user.id, auth_token: auth_token }
-      respond_with data, :location => root_url
+      response = { success: true, user_id: user.id, auth_token: auth_token }
+      respond_with response, :location => root_url
     else
-      # Why doesn't respond_with work?
-      respond_with "Error: User authentication failed.", status: 200, :location => root_url
+      response = { success: false, message: "Incorrect email or password." }
+      respond_with response, status: 401, :location => root_url
     end
   end
 
@@ -22,9 +22,11 @@ class SessionsController < ApplicationController
     auth_token = params[:auth_token]
     if user && user.authenticated?(auth_token)
       user.end_session
-      respond_with "Successfully logged out.", status: 200, :location => root_url
+      response = { success: true, message: "Successfully logged out." }
+      respond_with response, status: 200, :location => root_url
     else
-      respond_with "Error: Failed to logout.", status: 401, :location => root_url
+      response = { success: true, message: "Error: Failed to logout." }
+      respond_with response, status: 401, :location => root_url
     end
   end
 
