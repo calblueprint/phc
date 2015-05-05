93b973c666feecd0be690f026917c10a8eb33e8a
Alton Zheng-Xie <altonzheng@berkeley.edu>
2015-05-04 19:11:57 -0700

    Remove index on salesforce id

--- phc-rails/db/schema.rb
+++ phc-rails/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150501030944) do
+ActiveRecord::Schema.define(version: 20150505021012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 20150501030944) do
 
   add_index "accounts", ["FirstName"], name: "index_accounts_on_FirstName", using: :btree
   add_index "accounts", ["LastName"], name: "index_accounts_on_LastName", using: :btree
-  add_index "accounts", ["sf_id"], name: "index_accounts_on_sf_id", unique: true, using: :btree
 
   create_table "event_registrations", force: :cascade do |t|
     t.string   "account_sfid", limit: 255
