033c4ab41d19db64098f1e5c4dbac24f66570963
Alton Zheng-Xie <altonzheng@berkeley.edu>
2015-06-03 11:21:27 +0900

    Account factory

--- phc-rails/config/application.rb
+++ phc-rails/config/application.rb
@@ -21,5 +21,16 @@ module PhcRails
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        :fixtures => true,
+        :view_specs => true,
+        :helper_specs => true,
+        :routing_specs => true,
+        :controller_specs => true,
+        :request_specs => true
+      g.fixture_replacement :factory_girl, :dir => "spec/factories"
+    end
   end
 end
