8c58d6c03074f08bc9b8f6699fb8da78bab81e1a
Byron Zhang <byron.yi.zhang@gmail.com>
2016-03-22 23:08:47 -0700

    Clean up syntax and add Hearing Care event.

--- phc-rails/app/models/service.rb
+++ phc-rails/app/models/service.rb
@@ -18,47 +18,48 @@ class Service < ActiveRecord::Base
   def self.services
     # In the future, we may retrieve these through the Salesforce API, but
     # for now they are hardcoded in
-    services = [
-      "Acupuncture__c",
-      "Addiction_Recovery__c",
-      "Adult_Probation__c",
-      "Banking__c",
-      "Books__c",
-      "CAAP__c",
-      "CalFresh__c",
-      "Dental__c",
-      "Disability_Services__c",
-      "DMV_ID__c", "Employment__c",
-      "Family_Services__c",
-      "Foot_Washing__c",
-      "Groceries__c",
-      "Haircuts__c",
-      "HIV_STI_Testing__c",
-      "Homeward_Bound__c",
-      "Housing_Info__c",
-      "Legal__c",
-      "Lifeline_Cell__c",
-      "Fingerprints__c",
-      "Lunch__c",
-      "Mammograms__c",
-      "Massage__c",
-      "Medical__c",
-      "Mental_Health__c",
-      "Needle_Exchange__c",
-      "Pet_Care__c",
-      "Phone_Calls__c",
-      "Photo_Portraits__c",
-      "Vision_Readers__c",
-      "Senior_Services__c",
-      "SSI_SSDI_Medi_Cal__c",
-      "Veteran_Services__c",
-      "Vision_Screening__c",
-      "Voter_Registration__c",
-      "Wheelchair_Repair__c",
-      "Women_Services__c",
-      "Youth_Services__c"
-    ]
-    return services
+    services = %w(
+      Acupuncture__c
+      Addiction_Recovery__c
+      Adult_Probation__c
+      Banking__c
+      Books__c
+      CAAP__c
+      CalFresh__c
+      Dental__c
+      Disability_Services__c
+      DMV_ID__c Employment__c
+      Family_Services__c
+      Foot_Washing__c
+      Groceries__c
+      Haircuts__c
+      Hearing_Care__c
+      HIV_STI_Testing__c
+      Homeward_Bound__c
+      Housing_Info__c
+      Legal__c
+      Lifeline_Cell__c
+      Fingerprints__c
+      Lunch__c
+      Mammograms__c
+      Massage__c
+      Medical__c
+      Mental_Health__c
+      Needle_Exchange__c
+      Pet_Care__c
+      Phone_Calls__c
+      Photo_Portraits__c
+      Vision_Readers__c
+      Senior_Services__c
+      SSI_SSDI_Medi_Cal__c
+      Veteran_Services__c
+      Vision_Screening__c
+      Voter_Registration__c
+      Wheelchair_Repair__c
+      Women_Services__c
+      Youth_Services__c
+    )
+    services
   end
 
   def status_string
@@ -73,5 +74,4 @@ class Service < ActiveRecord::Base
       "Received"
     end
   end
-
 end
